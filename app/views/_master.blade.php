896e170fa228b82c2ddfed2f93f059afaec1abce
David Becerra <dbecerra@college.harvard.edu>
2014-07-13 15:54:11 -0400

    Added landing page route and blade.php files

---
+++ app/views/_master.blade.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html>
+<head>
+
+  <title>@yield('title', 'P3')</title>
+
+  @yield('head')
+
+</head>
+
+<body>
+
+  @yield('content')
+
+  @yield('body')
+
+</body>
+
+</html>
---
+++ app/views/index.blade.php
@@ -0,0 +1,5 @@
+@extends('_master')
+
+@section('content')
+	<h1>Homepage</h1>
+@stop
