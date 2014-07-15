299bd9735bd4bec9c0a0539d7ea2b135dfe7083b
David Becerra <dbecerra@college.harvard.edu>
2014-07-15 17:14:20 -0400

    Random user generator functionality done. Using outside dependency

--- app/routes.php
+++ app/routes.php
@@ -1,15 +1,15 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the Closure to execute when that URI is requested.
-|
-*/
+function clean_input($input) {
+	$input = trim($input);
+	$input = stripslashes($input);
+	$input = htmlspecialchars($input);
+	return $input;
+}
+
+function validate_input_number($input, $min, $max) {
+	return (is_numeric($input) && $input <= $max && $input >= $min);
+}
 
 // Homepage
 Route::get('/', function()
@@ -21,13 +21,11 @@ Route::get('/', function()
 Route::get('/lorem-ipsum', function() {
 
 	if (!empty($_GET)) {
-		// Clean form input
-		$paragraphs = trim($_GET['paragraphs']);
-		$paragraphs = stripslashes($paragraphs);
-		$paragraphs = htmlspecialchars($paragraphs);
+		// Scrub form input
+		$paragraphs = clean_input($_GET['paragraphs']);
 
-		// // Ensure paragraphs is integer
-		if (is_numeric($paragraphs) && $paragraphs <= 99 && $paragraphs > 0) {
+		// Ensure paragraphs is integer
+		if (validate_input_number($paragraphs, 1, 99)) {
 			$paragraphs = (int) $paragraphs;
 			$generator = new Badcow\LoremIpsum\Generator();
 			$output = implode('<p>', $generator->getParagraphs($paragraphs));
@@ -42,6 +40,20 @@ Route::get('/lorem-ipsum', function() {
 
 // Random User Generator
 Route::get('/rand-user', function() {
-	return View::make('rand_user');
+	$output = '';
+	if (!empty($_GET)) {
+		$num_users = clean_input($_GET['users']);
+
+		if (validate_input_number($num_users, 1, 9)) {
+			$num_users = (int) $num_users;
+			$fake_user = Faker\Factory::create();
+			for ($i=0; $i < $num_users; $i++) { 
+				$output .= $fake_user->name . "<br>";
+				$output .= $fake_user->address . "<br>";
+				$output .= $fake_user->text . "<br>";
+			}
+		}
+	}
+	return View::make('rand_user')->with('user_data', $output);
 });
 
--- app/views/rand_user.blade.php
+++ app/views/rand_user.blade.php
@@ -2,5 +2,9 @@
 
 @section('content')
 	<a href="/">Home</a>
-	This will be a random user Generator!
+	<form method="GET" action="">
+		Number of Users: <input type="text" name="users" required> (Max 9)<br>
+		<input type="submit" value="Submit">
+	</form>
+	<?php echo $user_data; ?>
 @stop
